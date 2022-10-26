08fb4b380e7cb3d67aa7fa453991f9e037cf1ad2
Davef <76170782+davefdev@users.noreply.github.com>
2022-10-26 19:09:48 +0100

    setup and navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { AiOutLineMenu } from "react-icons/ai";
+
+const Navbar = () => {
+  return <div className="max-w-[1640x] mx-auto flex justify-between items-center p-5">
+    
+    
+    
+    
+    
+    
+    Navbar</div>;
+};
+
+export default Navbar;
