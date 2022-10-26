e89c444eea2f5a430653c8828a58c5f072670993
Davef <76170782+davefdev@users.noreply.github.com>
2022-10-26 21:23:26 +0100

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,13 +12,15 @@ const Navbar = () => {
         <h1 className="text-2xl sm:text-3xl lg:text-4xl  px-2">
           Vegan <span className="font-bold">Munch</span>
         </h1>
+
         <div className="hidden lg:flex items-center bg-gray-300 rounded-full p-1 text[14px]">
           <p className="bg-black text-white rounded-full p-2">Delivery</p>
           <p className="p-2">Collection</p>
         </div>
       </div>
+
       {/*search input*/}
-      <div className="bg-gray-300 flex items-center px-2  w-[200px] sm:w-[400px] lg:w-[500px]">
+      <div className="bg-gray-300 flex items-center px-2 rounded-full w-[200px] sm:w-[400px] lg:w-[500px]">
         <AiOutlineSearch />
         <input
           className="bg-transparent p-2 focus:outline-none w-full"
