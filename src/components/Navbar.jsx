2a5af4a655d5bd039419c117c47820e9c767d334
Davef <76170782+davefdev@users.noreply.github.com>
2022-10-26 19:20:25 +0100

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,23 @@
 import React from "react";
-import { AiOutLineMenu } from "react-icons/ai";
+import { AiOutlineMenu } from "react-icons/ai";
 
 const Navbar = () => {
-  return <div className="max-w-[1640x] mx-auto flex justify-between items-center p-5">
-    
-    
-    
-    
-    
-    
-    Navbar</div>;
+  return (
+    <div className="max-w-[1640x] mx-auto flex justify-between items-center p-5">
+      <div>
+        <div>
+          <AiOutlineMenu size={32} />
+        </div>
+        <h1>
+          best <span>Munch</span>
+        </h1>
+        <div>
+          <p>Delivery</p>
+          <p>Collection</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
