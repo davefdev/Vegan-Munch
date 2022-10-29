3550651922ef727f48dd798788bea6cf4e8d1942
Davef <76170782+davefdev@users.noreply.github.com>
2022-10-29 12:41:25 +0100

    cards

--- src/components/HeadlineCards.jsx
+++ src/components/HeadlineCards.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React from "react";
 
 const HeadlineCards = () => {
   return (
-    <div>
-        
-        container
-    </div>
-  )
-}
+    <div className="max-w-[1640px] mx-auto p-4 grid md:grid-cols-3 gap5  ">
 
-export default HeadlineCards
+      {/*Card*/}
+      <div className=" rounded-xl relative">
+        {/*Overlay*/}
+        <div className=" ">
+          <p>Weather Hot, Food Got</p>
+          <p>Through October</p>
+
+          <button>Order now</button>
+        </div>
+        <img
+          src="https://images.pexels.com/photos/2955819/pexels-photo-2955819.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt="burrito"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default HeadlineCards;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,8 @@ const Hero = () => {
   return (
     <div className="max-w-[1640px] mx-auto p-4">
       <div className="max-h-[500px] relative">
-        {/*OVerlay*/}
+
+        {/*Overlay*/}
         <div className="absolute w-full h-full text-gray-200 max-h-[500px] bg-black/40 flex flex-col justify-center rounded-2xl">
           <h1 className="px-4 text-4xl sm:text-5xl md:text-6xl lg:7xl font-bold">
             Natural <span className="text-orange-500">Wholesome</span>{" "}
@@ -13,6 +14,7 @@ const Hero = () => {
             Foods <span className="text-orange-500">Delivered</span>{" "}
           </h1>
         </div>
+
         <img
           className="w-full max-h-[500px] object-cover rounded-2xl"
           src="https://images.pexels.com/photos/3296903/pexels-photo-3296903.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
