d75e66bf86db1cf57054ee50fbcfc6a5f15d6e5c
Davef <76170782+davefdev@users.noreply.github.com>
2022-10-29 09:38:29 +0100

    HERO COMPONENT

---
+++ src/components/Hero.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div>
+      <div>
+        {/*OVerlay*/}
+        <div>
+
+          
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
