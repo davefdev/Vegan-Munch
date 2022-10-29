cf210c6a38146de0dcf06f0d7f93a85a2ccef666
Davef <76170782+davefdev@users.noreply.github.com>
2022-10-29 13:39:19 +0100

    cards styling

--- src/components/HeadlineCards.jsx
+++ src/components/HeadlineCards.jsx
@@ -2,22 +2,58 @@ import React from "react";
 
 const HeadlineCards = () => {
   return (
-    <div className="max-w-[1640px] mx-auto p-4 grid md:grid-cols-3 gap5  ">
-
+    <div className="max-w-[1640px] mx-auto p-4 md:grid grid-cols-3 gap-5  ">
       {/*Card*/}
-      <div className=" rounded-xl relative">
+      <div className="rounded-2xl relative">
         {/*Overlay*/}
-        <div className=" ">
-          <p>Weather Hot, Food Got</p>
-          <p>Through October</p>
-
-          <button>Order now</button>
+        <div className="absolute w-full h-full bg-black/40 rounded-2xl text-white">
+          <p className="font-bold text-2xl px-2 pt-4">No animal products used</p>
+          <p className="px-2">Whatsoever!</p>
+          <button className="border-white bg-white text-black mx-2 absolute bottom-4">
+            Order now
+          </button>
         </div>
         <img
+          className="max-h-[200px] w-full object-cover rounded-2xl"
           src="https://images.pexels.com/photos/2955819/pexels-photo-2955819.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
           alt="burrito"
         />
       </div>
+
+      {/*Card*/}
+      <div className="rounded-2xl relative">
+        {/*Overlay*/}
+        <div className="absolute w-full h-full bg-black/40 rounded-2xl text-white">
+          <p className="font-bold text-2xl px-2 pt-4">New deals</p>
+          <p className="px-2">Weekly</p>
+          <button className="border-white bg-white text-black mx-2 absolute bottom-4">
+            Order now
+          </button>
+        </div>
+        <img
+          className="max-h-[200px] w-full object-cover rounded-2xl"
+          src="https://images.pexels.com/photos/1437267/pexels-photo-1437267.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt="pasta"
+        />
+      </div>
+
+      {/*Card*/}
+      <div className="rounded-2xl relative">
+        {/*Overlay*/}
+        <div className="absolute w-full h-full bg-black/40 rounded-2xl text-white">
+          <p className="font-bold text-2xl px-2 pt-4">Fresh, local produce</p>
+          <p className="px-2">Cooked to perfection</p>
+          <button className="border-white bg-white text-black mx-2 absolute bottom-4">
+            Order now
+          </button>
+        </div>
+        <img
+          className="max-h-[200px] w-full object-cover rounded-2xl"
+          src="https://images.pexels.com/photos/1618898/pexels-photo-1618898.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt="hummus"
+        />
+      </div>
+     
     </div>
   );
 };
