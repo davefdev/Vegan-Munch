e28469863a8389ee06c607db109b326bddf0ad46
Davef <76170782+davefdev@users.noreply.github.com>
2022-10-30 10:04:12 +0000

    Map and Filter functions for food search

---
+++ src/components/Category.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { categories } from "../data/data";
+
+const Category = () => {
+  return (
+    <div>
+      <h1>Top rated items</h1>
+    </div>
+  );
+};
+
+export default Category;
